1974b3e75a42de275cfadf8b00da52d3a17a7c31
Maya <maya_sato@diveintocode.jp>
2018-05-19 17:55:34 +0900

    共通処理のメソッド化

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update]
+
   def index
     @tweets = Tweet.all
   end
@@ -34,7 +36,12 @@ class TweetsController < ApplicationController
   end
 
   private
-  def tweet_params
-    params.require(:tweet).permit(:content)
-  end
+    def tweet_params
+      params.require(:tweet).permit(:content)
+    end
+
+    # idをキーとして値を取得するメソッド
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
+    end
 end
